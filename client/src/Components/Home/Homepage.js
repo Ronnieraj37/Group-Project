8ad76bc4661b8d3819950980a97c3b01b4e9ac37
Ronnieraj37 <101713157+Ronnieraj37@users.noreply.github.com>
2024-05-06 18:25:17 +0530

    fixed lottie

--- client/src/Components/Home/Homepage.js
+++ client/src/Components/Home/Homepage.js
@@ -12,8 +12,6 @@ import Loader from "../Helpers/Loader";
 import Logo from "../Cards/Logo";
 import Push from "../Cards/Push";
 import "./LandingPage.scss";
-import Lottie from "react-lottie";
-import animationData from "./Lottie/security-research.json";
 const Homepage = ({ setconnected }) => {
   const [signers, setsigners] = useState(null);
   const [connect, setconnect] = useState(false);
@@ -447,25 +445,7 @@ const Homepage = ({ setconnected }) => {
                   access can use them.
                 </div>
               </div>
-              <div className="container__content__anim">
-                {/* <script src='./security-research.json'></script> */}
-                {/* <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-
-      <lottie-player src="https://lottie.host/6f4ee854-3625-4849-8985-e423a0752949/hZWZsrb0zk.json" background="transparent" speed="1" style={{width: '300px', height: '300px'}} direction="1" mode="normal" loop controls autoplay>
-      </lottie-player> */}
-                <Lottie
-                  options={{
-                    loop: true,
-                    autoplay: true,
-                    animationData: animationData,
-                    rendererSettings: {
-                      preserveAspectRatio: "xMidYMid slice",
-                    },
-                  }}
-                  height={400}
-                  width={450}
-                />
-              </div>
+              <div className="container__content__anim"></div>
             </div>
           </div>
         </>
--- client/src/Components/Home/LandingPage.js
+++ client/src/Components/Home/LandingPage.js
@@ -1,39 +1,22 @@
-import React from 'react'
-import './LandingPage.scss'
-import Lottie from 'react-lottie';
-import animationData from './Lottie/security-research.json';
+import React from "react";
+import "./LandingPage.scss";
 
 const LandingPage = () => {
   return (
-    <div className='container'>
-        <div className='container__text'>
-            <div className='header'>
-            Sentinals of Your Confidentiality: Protecting Your Important Documents
-            </div>
-            <div className='summary'>
-            Take control of the privacy of your precious documents by using InfoSentinel, ensuring that only those you've granted access can use them.
-            </div>
+    <div className="container">
+      <div className="container__text">
+        <div className="header">
+          Sentinals of Your Confidentiality: Protecting Your Important Documents
         </div>
-        <div className='container__anim'>
-            {/* <script src='./security-research.json'></script> */}
-            {/* <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-
-            <lottie-player src="https://lottie.host/6f4ee854-3625-4849-8985-e423a0752949/hZWZsrb0zk.json" background="transparent" speed="1" style={{width: '300px', height: '300px'}} direction="1" mode="normal" loop controls autoplay>
-            </lottie-player> */}
-            <Lottie 
-                options={{loop: true,
-                    autoplay: true,
-                    animationData: animationData,
-                    rendererSettings: {
-                      preserveAspectRatio: "xMidYMid slice"
-                    }
-                  }}
-                height={400}
-                width={450}
-            />
+        <div className="summary">
+          Take control of the privacy of your precious documents by using
+          InfoSentinel, ensuring that only those you've granted access can use
+          them.
         </div>
+      </div>
+      <div className="container__anim"></div>
     </div>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
